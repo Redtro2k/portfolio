0a81505f1f9108a07873a655bda587e087b47b8e
Blu369 <llacuna.darwin24@gmail.com>
2023-07-01 22:52:33 +0800

    add colorful background

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 </script>
 <template>
-  <div class="bg-gray-100 h-screen">
-      <div class="mx-48 bg-white h-screen">
+  <div class="bg-gray-100 h-screen bg-gradient-to-r from-gray-100 to-slate-100">
+      <div class="mx-48 bg-gradient-to-r from-indigo-500 via-cyran-500 to-sky-500 h-screen">
           <!-- navigation -->
           <div class="p-2 mx-14 flex items-center justify-between">
-            <h1 class="leading-loose tracking-2 text-5xl text-zinc-600 font-belanosima">
-              <span class="font-bold">D</span>
-              <span class="text-xl text-rose-600 font-semibold">L</span>
+            <h1 class="leading-loose tracking-2 text-6xl text-white font-belanosima">
+              <span class="font-bold text-cyan-400 drop-shadow-lg">D</span>
+              <span class="text-xl text-sky-500 font-semibold drop-shadow-lg">L</span>
             </h1>
-            <ul class="flex space-x-4 border-solid border px-4 py-2 rounded-full shadow-md font-figree text-sm">
+            <ul class="flex space-x-4 border-solid border px-4 py-2 rounded-full shadow-md font-figree text-sm text-white">
               <li>Home</li>
               <li>Objective</li>
               <li>Educations</li>
@@ -19,15 +19,24 @@
           </div>
           <!-- main content -->
           <div class="flex items-center mx-24">
-            <img src="./assets/images.png" class="saturate-50 h-1/2" alt="">
+            <img src="./assets/images.png" class="saturate-50 h-1/2  drop-shadow-md" alt="">
             <div class="mx-4 mt-8 max-w-md text-left font-belanosima space-y-2">
-              <h5 class="uppercase font-semibold text-gray-300">Hey there</h5>
-              <h2 class="text-5xl font-semibold text-neutral-700">I'm <span class="text-rose-600 stroke-1">D</span>arwin Llacuna</h2>
-              <h3 class="font-semibold text-gray-500">Web Developer</h3>
-              <span class="text-sm text-gray-500 font-figree">I am a fresh graduate student who graduated from STI College Fairview. I have experience creating full-stack websites such as CMS and Portal.
+              <h5 class="uppercase font-semibold text-sky-300">Hey there</h5>
+              <h2 class="text-5xl font-semibold text-gray-50 drop-shadow-lg animate-bounce">I'm <span class="text-gray-600 stroke-1">D</span>arwin Llacuna</h2>
+              <h3 class="font-semibold text-sky-300">Web Developer</h3>
+              <span class="text-sm text-indigo-100 font-figree">I am a fresh graduate student who graduated from STI College Fairview. I have experience creating full-stack websites such as CMS and Portal.
               </span>
-              <ul>
-                <li><font-awesome-icon icon="fa-solid fa-user-secret" /></li>
+              <ul class="flex space-x-4">
+                <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-facebook h-7 w-7 text-gray-200 hover:text-sky-400 hover:opacity-75" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                  </svg>
+                </li>
+                <li>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-github h-7 w-7 text-gray-200 hover:text-gray-700 hover:opacity-75" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                  </svg>
+                </li>
               </ul>
             </div>
           </div>

fe0fd41a1dd96da1ae1d8edcfc87668dbd0734bb
Blu369 <llacuna.darwin24@gmail.com>
2023-07-02 15:13:58 +0800

    deploy dist 7

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
 
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./Portfolio-app/', import.meta.url))
     }
   }
 })

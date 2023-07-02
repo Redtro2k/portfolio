033e2db6e8b7607913a2798984fc476f7e0f9e6a
Blu369 <llacuna.darwin24@gmail.com>
2023-07-02 15:13:04 +0800

    deploy dist 6

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/Portfolio-app/',
   plugins: [
     vue(),
   ],
+    base: '/Portfolio-app/',
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
